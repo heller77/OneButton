a74b70d90b2f3943f7e5b6eacba91ac4a73574cd
heller77 <koki001230@icloud.com>
2023-09-22 01:55:05 +0900

    スペースボタンを押すとボタンが押されるアニメーションが再生されるように

---
+++ Assets/Scripts/Character/ButtonAnimationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Character
+{
+    public class ButtonAnimationManager : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _animator.SetBool("push", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                _animator.SetBool("push", false);
+            }
+        }
+    }
+}
