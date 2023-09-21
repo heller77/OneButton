fc186b179f6eaafd6a05913a520c7c7727b8c111
heller77 <koki001230@icloud.com>
2023-09-21 23:32:02 +0900

    initial commit

---
+++ Assets/Scripts/Character/MoverOnSpline.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace Character
+{
+    public class MoverOnSpline : MonoBehaviour
+    {
+        [SerializeField] private SplineContainer spline;
+
+        // 経路上の位置
+        [SerializeField, Range(0, 1)] private float _t;
+
+        private void Update()
+        {
+        }
+#if UNITY_EDITOR
+
+        // インスペクターから編集されたとき
+        private void OnValidate()
+        {
+            MoveOnSpline();
+        }
+
+#endif
+
+        private void MoveOnSpline()
+        {
+            // スプライン上の位置・向き・上ベクトルを取得
+            if (!spline.Evaluate(_t, out var position, out var tangent, out var upVector))
+                return;
+            // Transformに反映
+            transform.SetPositionAndRotation(
+                position,
+                Quaternion.LookRotation(tangent, upVector)
+            );
+        }
+    }
+}
