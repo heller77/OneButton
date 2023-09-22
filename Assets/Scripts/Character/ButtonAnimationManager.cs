5d3342c2b982376061d9b2e3d26ff407af16f82c
heller77 <koki001230@icloud.com>
2023-09-22 16:21:21 +0900

    ボタンを押したらボタンの色が変わるように

--- Assets/Scripts/Character/ButtonAnimationManager.cs
+++ Assets/Scripts/Character/ButtonAnimationManager.cs
@@ -5,18 +5,35 @@ namespace Character
 {
     public class ButtonAnimationManager : MonoBehaviour
     {
-        [SerializeField] private Animator _animator;
+        [SerializeField] private Animator animator;
+        [SerializeField] private MeshRenderer _meshRenderer;
+
+        [SerializeField] private Material nonPushedMaterial;
+
+        [SerializeField] private Material pushedMaterial;
+        [SerializeField] private Light buttonLight;
+        
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                _animator.SetBool("push", true);
+                animator.SetBool("push", true);
             }
             else if (Input.GetKeyUp(KeyCode.Space))
             {
-                _animator.SetBool("push", false);
+                animator.SetBool("push", false);
             }
+        }
+
+        public void ChangeMaterial_WhenPushing()
+        {
+            _meshRenderer.material = pushedMaterial;
+        }
+
+        public void ChangeMaterial_WhenNoPushing()
+        {
+            _meshRenderer.material = nonPushedMaterial;
         }
     }
 }
