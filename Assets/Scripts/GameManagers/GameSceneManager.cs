af79a5d26c98c6c9da76135a1129ceee8c3ca6c0
heller77 <koki001230@icloud.com>
2023-09-24 23:31:59 +0900

    タイトルシーンからゲームシーンに遷移できるようにした

--- Assets/Scripts/GameManagers/GameSceneManager.cs
+++ Assets/Scripts/GameManagers/GameSceneManager.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameManagers
 {
     public class GameSceneManager : MonoBehaviour
     {
-        
+        private void Start()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        /// <summary>
+        /// Mainシーンに遷移
+        /// </summary>
+        public void GoToInGameScene()
+        {
+            SceneManager.LoadScene("Main");
+        }
     }
 }
