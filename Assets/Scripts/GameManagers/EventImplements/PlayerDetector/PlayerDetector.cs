439ec6955882e3c09560e2dc00d90afb8f4e85e2
heller77 <koki001230@icloud.com>
2023-10-29 21:36:15 +0900

    プレイヤーがある範囲に来たら検知するクラス実装

---
+++ Assets/Scripts/GameManagers/EventImplements/PlayerDetector/PlayerDetector.cs
@@ -0,0 +1,33 @@
+﻿using Character;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace GameManagers.EventImplements.PlayerDetector
+{
+    /// <summary>
+    /// プレイヤーを検知する
+    /// </summary>
+    public class PlayerDetector : MonoBehaviour
+    {
+        private bool isDetect;
+
+        public async UniTask WaitDetect()
+        {
+            await UniTask.WaitUntil(() => isDetect);
+        }
+
+        public void Detect()
+        {
+            isDetect = true;
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<PlayerRobotManager>(out var player))
+            {
+                Detect();
+            }
+        }
+    }
+}
