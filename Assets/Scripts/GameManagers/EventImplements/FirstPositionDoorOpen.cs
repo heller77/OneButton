871df14629faa78f8099ce29b384d7b0f71d3bf8
heller77 <koki001230@icloud.com>
2023-10-29 21:41:09 +0900

    ドアを開けるためのスクリプト作成

---
+++ Assets/Scripts/GameManagers/EventImplements/FirstPositionDoorOpen.cs
@@ -0,0 +1,13 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace GameManagers.EventImplements
+{
+    public class FirstPositionDoorOpen : MonoBehaviour
+    {
+        public async UniTask OpenDoor()
+        {
+            Debug.Log("open");
+        }
+    }
+}
