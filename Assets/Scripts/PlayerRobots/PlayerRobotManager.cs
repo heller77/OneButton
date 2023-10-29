92222d28abd9f1f9b6b4668656fbc81e4d9c2a87
heller77 <koki001230@icloud.com>
2023-10-29 21:43:24 +0900

    プレイヤーの移動をスクリプトから管理するように

--- Assets/Scripts/PlayerRobots/PlayerRobotManager.cs
+++ Assets/Scripts/PlayerRobots/PlayerRobotManager.cs
@@ -1,9 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.Splines;
+using Utils;
 
 namespace Character
 {
     public class PlayerRobotManager : MonoBehaviour
     {
-        
+        // [SerializeField] private SplineAnimate _splineAnimate;
+        [SerializeField] private MoverOnSpline _moverOnSpline;
+
+        /// <summary>
+        /// 動きをスタート
+        /// </summary>
+        public void StartMove()
+        {
+            Debug.Log("start move");
+            _moverOnSpline.Play();
+        }
+
+        /// <summary>
+        /// 動きを止める
+        /// </summary>
+        public void StopMove()
+        {
+            _moverOnSpline.Pause();
+        }
     }
 }
