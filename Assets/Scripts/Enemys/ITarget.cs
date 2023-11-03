a98e085094958be75818937db37947e1c20df771
heller77 <koki001230@icloud.com>
2023-11-04 01:47:48 +0900

    ロックオンする用のカーソルを表示できるように

---
+++ Assets/Scripts/Enemys/ITarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Enemys
+{
+    /// <summary>
+    /// ターゲット
+    /// </summary>
+    public interface ITarget
+    {
+        /// <summary>
+        /// ターゲットの位置を返す
+        /// </summary>
+        /// <returns>ターゲットの位置</returns>
+        Vector3 GetPosition();
+    }
+}
