371d997cfaf640c6094af47520eeb0207c0bfe6c
heller77 <koki001230@icloud.com>
2023-11-12 21:16:01 +0900

    MobEnemyクラスのHPをScriptableObjectで設定できるようにした。

---
+++ Assets/Scripts/Enemys/EnemyParameter/EnemyParameterAsset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Enemys.EnemyParameter
+{
+    [CreateAssetMenu(fileName = "enemydata", menuName = "ScriptableObjects/CreateEnemyParameter")]
+    public class EnemyParameterAsset : ScriptableObject
+    {
+        [SerializeField] private int _maxhp;
+
+        public int maxhp => _maxhp;
+    }
+}
--- Assets/Scripts/Enemys/MobEnemy.cs
+++ Assets/Scripts/Enemys/MobEnemy.cs
@@ -1,20 +1,32 @@
 ﻿using System;
+using Enemys.EnemyParameter;
 using UnityEngine;
 
 namespace Enemys
 {
-    public class MobEnemy : MonoBehaviour, ITarget
+    public class MobEnemy : MonoBehaviour, ITarget, IHitable
     {
         [SerializeField] private EnemyManager _enemyManager;
+
+        [SerializeField] private EnemyParameterAsset _parameterAsset;
+        [SerializeField] private int hp;
 
         private void Start()
         {
             _enemyManager.Add(this);
+            hp = _parameterAsset.maxhp;
         }
 
         public Vector3 GetPosition()
         {
             return this.transform.position;
+        }
+
+        /// <summary>
+        /// 攻撃された
+        /// </summary>
+        public void Hitted()
+        {
         }
     }
 }
