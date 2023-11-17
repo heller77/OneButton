e520f6b728ba7eda2a03b7183a6c9f440b4c08ef
heller77 <koki001230@icloud.com>
2023-11-17 18:00:40 +0900

    inputsystemを使ってタイトルシーンからゲームシーン（Main）に遷移するようにした

---
+++ Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using GameManagers;
+using MyInputs;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TitleScene
+{
+    public class TitleSceneManager : MonoBehaviour
+    {
+        private GameInputs input;
+
+        [SerializeField] private GameSceneManager _gameSceneManager;
+
+        private void Start()
+        {
+            input = new GameInputs();
+            input.Player.PushButton.started += GoToInGame;
+            input.Enable();
+        }
+
+        /// <summary>
+        /// インゲームを
+        /// </summary>
+        /// <param name="obj"></param>
+        private void GoToInGame(InputAction.CallbackContext obj)
+        {
+            Debug.Log("gotogame");
+            _gameSceneManager.GoToInGameScene();
+            input.Disable();
+        }
+    }
+}
