ab6ddd612a9a30c53a5e04d6d9915f241e8843df
heller77 <koki001230@icloud.com>
2023-12-27 16:54:30 +0900

    自作の飛行機でBoids実装した

---
+++ Assets/Scripts/Enemys/Boids/BoidParameter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Enemys.Boids
+{
+    [CreateAssetMenu(menuName = "Boid/Param")]
+    public class BoidParameter : ScriptableObject
+    {
+        public int boidsCount;
+
+        public float minSpeed;
+        public float maxSpeed;
+
+        public float nearboidsDistance;
+
+        /// <summary>
+        /// 行動を制限する球の半径
+        /// </summary>
+        public float restrictionSphereRadius;
+
+        public float restrictionStartDistance;
+
+        /// <summary>
+        /// Boidsの中心へと向かう力（遠いほど強くなるように計算される）
+        /// restrictionStartDistanceより
+        /// </summary>
+        public float powerToBoidsCenter;
+
+        public float separetePower;
+        public float alignmentPower;
+        public float cohesionPower;
+    }
+}
--- Assets/Scripts/Enemys/Boids/BoidsManager.cs
+++ Assets/Scripts/Enemys/Boids/BoidsManager.cs
@@ -1,68 +1,174 @@
-﻿using System.Collections;
-using Unity.Collections;
-using Unity.Jobs;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Enemys.Boids
 {
+    /// <summary>
+    ///     Boidsクラスを管理
+    /// </summary>
     public class BoidsManager : MonoBehaviour
     {
-        private struct Boid : IJob
+        private readonly List<Boid> boidList = new List<Boid>();
+        [SerializeField] private GameObject boidPrefab;
+        [SerializeField] private BoidParameter _boidParameter;
+
+        private void Start()
         {
-            public NativeArray<float> numbersToAdd;
-            public NativeArray<float> result;
-
-            public Boid(NativeArray<float> numbersToAdd, float result)
+            for (int i = 0; i < _boidParameter.boidsCount; i++)
             {
-                this.numbersToAdd = numbersToAdd;
-                this.result = new NativeArray<float>(1, Allocator.TempJob);
-            }
-
-            public void Execute()
-            {
-                float sumvalue = 0.0f;
-                for (int i = 0; i < numbersToAdd.Length; i++)
-                {
-                    sumvalue += numbersToAdd[i];
-                }
-                numbersToAdd[0] = sumvalue;
-                result[0] = sumvalue;
+                AddBoid();
             }
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                execute();
-            }
+            ControlBoids();
         }
 
-        private void execute()
+        /// <summary>
+        /// Boidを生成
+        /// </summary>
+        public void AddBoid()
         {
-            int length = 1000;
-            NativeArray<float> numbers = new NativeArray<float>(length, Allocator.TempJob);
-            ArrayList temparray = new ArrayList();
-            for (int i = 0; i < length; i++)
-            {
-                temparray.Add(i);
-            }
-
-            for (int i = 0; i < length; i++)
-            {
-                numbers[i] = temparray.IndexOf(i);
-            }
-
-            Boid boid = new Boid(numbers, 0.0f);
-            JobHandle handle = boid.Schedule();
-            handle.Complete();
-
-            //処理結果（）
-            Debug.Log(boid.numbersToAdd[0]); //55
-            Debug.Log(boid.result[0]); //0(処理結果入ってなさそう)
-
-            numbers.Dispose();
+            GameObject boid = Instantiate(boidPrefab, 10 * Random.onUnitSphere, Random.rotation);
+            var boidcomponent = boid.GetComponent<Boid>();
+            boidList.Add(boidcomponent);
+            boidcomponent.SetBoidParameter(this._boidParameter);
+            //初期加速度設定
+            Vector3 initAccel = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+            boidcomponent.SetAcceleration(boid.transform.forward);
         }
 
+        /// <summary>
+        /// Boidsの加速度を計算、設定
+        /// </summary>
+        public void ControlBoids()
+        {
+            foreach (Boid boid in boidList)
+            {
+                Vector3 preAccel = boid.GetAcceleration();
+
+                var powerToCenter = CalculateRestrictionSpherePower(boid.transform.position);
+
+                var nearboids = GetNearBoids(boid);
+                if (nearboids.Count > 0)
+                {
+                    var separatePower = CalculateSeparatePower(boid, nearboids);
+                    var alignmentPower = CalculateAlignmentPower(nearboids);
+                    var cohesionPower = CalculateCohesion(boid, nearboids);
+
+
+                    Vector3 newAccel = preAccel + powerToCenter + separatePower + alignmentPower + cohesionPower;
+                    boid.SetAcceleration(newAccel);
+                }
+                else
+                {
+                    Vector3 newAccel = preAccel + powerToCenter;
+                    boid.SetAcceleration(newAccel);
+                }
+            }
+        }
+
+        private Vector3 CalculateRestrictionSpherePower(Vector3 boidPosition)
+        {
+            float radius = _boidParameter.restrictionSphereRadius;
+            Vector3 boidsCenter = transform.position;
+
+            Vector3 vectorFromboidToboidcenter = boidsCenter - boidPosition;
+            float distance = vectorFromboidToboidcenter.magnitude;
+            if (_boidParameter.restrictionStartDistance < distance)
+            {
+                float powerMulti = (distance - _boidParameter.restrictionStartDistance) /
+                                   (_boidParameter.restrictionSphereRadius - _boidParameter.restrictionStartDistance);
+                return powerMulti * _boidParameter.powerToBoidsCenter * vectorFromboidToboidcenter;
+            }
+
+            return Vector3.zero;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private List<Boid> GetNearBoids(Boid targetBoid)
+        {
+            List<Boid> returnNearBoidList = new List<Boid>();
+            var prodThresh = Mathf.Cos(70 * Mathf.Deg2Rad);
+
+            foreach (var boid in this.boidList)
+            {
+                if (boid == targetBoid)
+                {
+                    continue;
+                }
+
+                var boidToOther = boid.transform.position - targetBoid.transform.position;
+                var distance_BoidToOther = boidToOther.magnitude;
+
+                if (distance_BoidToOther < _boidParameter.nearboidsDistance)
+                {
+                    var dot = Vector3.Dot(targetBoid.GetVelocity().normalized, boidToOther.normalized);
+                    if (prodThresh < dot)
+                    {
+                        returnNearBoidList.Add(boid);
+                    }
+                }
+            }
+
+            return returnNearBoidList;
+        }
+
+        private Vector3 CalculateSeparatePower(Boid targetBoid, List<Boid> nearBoid)
+        {
+            Vector3 power = Vector3.zero;
+
+            foreach (var boid in nearBoid)
+            {
+                power += (targetBoid.transform.position - boid.transform.position).normalized;
+            }
+
+            power /= nearBoid.Count;
+            return power * _boidParameter.separetePower;
+        }
+
+        private Vector3 CalculateAlignmentPower(List<Boid> nearBoid)
+        {
+            Vector3 alignmentPower = Vector3.zero;
+            foreach (var boid in nearBoid)
+            {
+                alignmentPower += boid.GetVelocity();
+            }
+
+            alignmentPower /= nearBoid.Count;
+            return alignmentPower * _boidParameter.alignmentPower;
+        }
+
+        private Vector3 CalculateCohesion(Boid target, List<Boid> nearBoid)
+        {
+            var cohesionPower = Vector3.zero;
+
+            var centerNearBoid = Vector3.zero;
+            foreach (var boid in nearBoid)
+            {
+                centerNearBoid += boid.transform.position;
+            }
+
+            centerNearBoid /= nearBoid.Count;
+
+            cohesionPower = (centerNearBoid - target.transform.position);
+            return cohesionPower * _boidParameter.cohesionPower;
+        }
+
+
+        /// <summary>
+        /// ギズモを表示
+        /// </summary>
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _boidParameter.restrictionSphereRadius);
+        }
     }
 }
