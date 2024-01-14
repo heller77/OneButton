4874ea56dc2558bd3cd4261dac0d651fceebf13f
heller77 <koki001230@icloud.com>
2024-01-14 18:07:33 +0900

    プレイヤーのチャージ率を表す球を管理するクラス

---
+++ Assets/Scripts/PlayerRobots/PlayerBulletSphereController/PlayerBulletSphereController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character.PlayerBulletController
+{
+    /// <summary>
+    /// 弾のチャージの率を表す球の管理
+    /// </summary>
+    public class PlayerBulletSphereController : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> sphereList = new List<GameObject>();
+        private int nowDisplaySphereNum = 0;
+
+        public void AddBall()
+        {
+            sphereList[nowDisplaySphereNum].SetActive(true);
+            this.nowDisplaySphereNum++;
+        }
+
+        public void HideAllSphere()
+        {
+            foreach (var sphere in sphereList)
+            {
+                sphere.SetActive(false);
+            }
+        }
+    }
+}
