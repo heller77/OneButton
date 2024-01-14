9e9a9ad578053611100318a534fa79c9330df74f
heller77 <koki001230@icloud.com>
2024-01-15 00:33:19 +0900

    ボスを倒したときの演出を実装

--- Assets/Scripts/Enemys/BossEnemy.cs
+++ Assets/Scripts/Enemys/BossEnemy.cs
@@ -1,12 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
 
 namespace Enemys
 {
     public class BossEnemy : MonoBehaviour, ITrapable
     {
+        [SerializeField] private GameObject defaultObject;
+
+        [SerializeField] private GameObject destructionObject;
+
+        [SerializeField] private List<Rigidbody> parts;
+        [SerializeField] private float destructionPower = 1.0f;
+        [SerializeField] private Transform powerOriginTransform;
+        [SerializeField] private ParticleSystem _particleSystem;
+
+        private void Start()
+        {
+            Destruction();
+        }
+
         public void Boot()
         {
-            
+        }
+
+        public void Destruction()
+        {
+            _particleSystem.Play();
+            defaultObject.SetActive(false);
+            destructionObject.SetActive(true);
+            foreach (var part in parts)
+            {
+                var powerDir = part.transform.position - powerOriginTransform.transform.position;
+                Debug.Log(powerDir);
+                part.AddForce(destructionPower * powerDir);
+            }
         }
     }
 }
