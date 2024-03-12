cf5d006a9ce3320a160bb34f6bbf13763beda3d2
heller77 <koki001230@icloud.com>
2024-03-12 21:34:52 +0900

    ライセンス表示できるように

---
+++ Assets/Scripts/TitleScene/UIs/DisplayLiscense.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TitleScene.UIs
+{
+    public class DisplayLiscense : MonoBehaviour
+    {
+        [SerializeField] private GameObject displayGameObjcet;
+
+        public void Display()
+        {
+            displayGameObjcet.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            displayGameObjcet.SetActive(false);
+        }
+    }
+}
