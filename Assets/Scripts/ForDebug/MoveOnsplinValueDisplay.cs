47b22af8f3fde5f3d4649f34da01797ce2f9260f
heller77 <koki001230@icloud.com>
2024-05-11 17:43:28 +0900

    MoveOnsplinValueDisplayクラスのコメント追加
    デバッグ機能のコミットで追加忘れ

--- Assets/Scripts/ForDebug/MoveOnsplinValueDisplay.cs
+++ Assets/Scripts/ForDebug/MoveOnsplinValueDisplay.cs
@@ -2,6 +2,9 @@
 
 namespace ForDebug
 {
+    /// <summary>
+    /// MoveOnsplineクラスのあるオブジェクトにつけると、今のMoveOnsplineの値を表示
+    /// </summary>
     public class MoveOnsplinValueDisplay : DisplayTextInEditor
     {
 #if UNITY_EDITOR
